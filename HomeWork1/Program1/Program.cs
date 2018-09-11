9d890b0ade33d875e7a5b52d3f19bf11308e2551
ZhaoChao <1162974108@qq.com>
2018-09-12 00:07:30 +0800

    first commit

---
+++ HomeWork1/Program1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b, c;
+            Console.Write("a:");
+            a = Double.Parse(Console.ReadLine());
+            Console.Write("b:");
+            b = Double.Parse(Console.ReadLine());
+            c = a * b;
+            Console.WriteLine("a*b={0:f}", c);
+        }
+    }
+}
