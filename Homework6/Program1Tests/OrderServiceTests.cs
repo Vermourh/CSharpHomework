25972ecea2dc6183e8fb7655ac2134baf43eb70d
ZhaoChao <1162974108@qq.com>
2018-10-24 21:53:54 +0800

    6th commit

---
+++ Homework6/Program1Tests/OrderServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        [TestMethod()]
+        public void AddOrderTest()
+        {
+            OrderService obj = new OrderService();
+            
+        }
+
+        [TestMethod()]
+        public void DelOrderTest()
+        {
+            OrderService obj = new OrderService();
+        }
+
+        [TestMethod()]
+        public void ModOrderTest()
+        {
+            OrderService obj = new OrderService();
+        }
+
+        [TestMethod()]
+        public void FindOrderByNumsTest()
+        {
+            OrderService obj = new OrderService();
+        }
+
+        [TestMethod()]
+        public void FindOrderByGoodsTest()
+        {
+            OrderService obj = new OrderService();
+        }
+
+        [TestMethod()]
+        public void FindOrderByNameTest()
+        {
+            OrderService obj = new OrderService();
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+            OrderService obj = new OrderService();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            OrderService obj = new OrderService();
+        }
+    }
+}
